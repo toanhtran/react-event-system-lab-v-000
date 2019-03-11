54d276968dde114c3b2257324ff6c35a5e57cc4c
ToAnh Tran <toanh.t.tran@gmail.com>
2019-03-11 19:13:21 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,34 @@
-// Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Components {
+  constructor(props){
+    super(props);
+    this.state = {
+      inout: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    handleChange(event){
+      this.setState({
+        input: event.target.value
+      });
+    }
+
+    handleSubmit(event) {
+      event.preventDefault();
+      this.setState({
+        input: '',
+        submit: this.state.input
+      });
+    }
+
+      render(){
+        return(
+
+        )
+      }
+    }
+}
+
+import default Keypad;
